dbe283321d88be2e55c14930d6994dbe8ed3145a
JaroslawK2020 <jaroslaw.kornatowski@gmail.com>
2021-11-14 15:48:04 +0100

    Initial commit

---
+++ src/main/java/com/sda/demotycopy/jarekk/DemotyApp.java
@@ -0,0 +1,13 @@
+package com.sda.demotycopy.jarekk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotyApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/demotycopy/jarekk/DemotyAppTests.java
@@ -0,0 +1,13 @@
+package com.sda.demotycopy.jarekk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotyAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
